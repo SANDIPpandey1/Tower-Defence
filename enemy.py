c64066db74ae9ca235f1564829401a1eb254e5e6
SANDIPpandey1 <102663858+SANDIPpandey1@users.noreply.github.com>
2024-05-29 14:59:05 +0545

    Day 2 : waypoint created and enemy follows the waypoint

--- enemy.py
+++ enemy.py
@@ -1,9 +1,28 @@
 import pygame as pg
+from pygame.math import Vector2
 import constant as c
 class Enemy(pg.sprite.Sprite):
-    def __init__(self, pos, image):
+    def __init__(self, waypoints, image):
         pg.sprite.Sprite.__init__(self)
+        self.waypoints = waypoints
+        self.pos = Vector2(self.waypoints[0])
+        self.target_waypoint = 1
+        self.speed = 2
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.center = pos
+        self.rect.center = self.pos
         
+    def update(self):
+        self.move()
+        
+    def move(self):
+        #define a target wavepoint 
+        self.target = Vector2(self.waypoints[self.target_waypoint])
+        self.movement = self.target - self.pos
+        #calculate target distance
+        dist = self.movement.length()
+    
+        #check the remaining dstance greater than enemy speed
+        if dist >= self.speed:
+            self.pos += self.movement.normalize() * self.speed
+        self.rect.center = self.pos      
--- main.py
+++ main.py
@@ -20,16 +20,47 @@ enemy_image = pg.image.load('assets\\enemy\\enemy_1.png').convert_alpha()
 #create enemy group
 enemy_group = pg.sprite.Group()
 
-enemy = Enemy((200,300), enemy_image)
+
+waypoints = [
+    (100, 100),
+    (400, 200),
+    (400, 100),
+    (200, 300)
+]
+
+enemy = Enemy(waypoints, enemy_image)
 enemy_group.add(enemy)
 
 
 run = True
 while run:
     clock.tick(c.FPS)
+    
+    #fill screen with color
+    screen.fill("white")
+    
+    #draw enemy path
+    pg.draw.lines(screen, "grey0", False, waypoints)
+    
+    
+    #update group
+    enemy_group.update()
+    
+    
+    
+    #draw a group
+    enemy_group.draw(screen)
+    
+    
+    
     #event handler 
     for event in pg.event.get():
+        
         #quite game
         if event.type == pg.QUIT:
             run = False
+            
+            
+    #update display
+    pg.display.flip()
 pg.quit()
